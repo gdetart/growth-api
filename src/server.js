e3743a467db574d71eb0923b565275819b507088
Detart <63840243+gdetart@users.noreply.github.com>
2020-11-17 22:33:15 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -27,13 +27,7 @@ const whitelist =
     : ["http://localhost:3000", "http://localhost:3003"];
 
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: "*",
   allowedHeaders: "*",
 };
 
